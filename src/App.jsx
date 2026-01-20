b0e37a319e91fba28f15e9a89bbb2a52412f10d1
Md Faisal Hussain <mdfaisalhussainm@gmail.com>
2026-01-20 11:09:46 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import React from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+        <div className='bg-red-500'>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa ratione aliquam eaque earum laudantium quas, sequi nobis optio modi quod eum possimus fugiat officiis pariatur magni autem ea ullam necessitatibus rem, neque beatae eos recusandae?
+        </div>
+    </>
+  )
+}
+
+export default App
