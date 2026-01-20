5beae26eddd72d6b291fa94e56691b3e3ab8db92
Md Faisal Hussain <mdfaisalhussainm@gmail.com>
2026-01-20 11:38:58 +0530

    changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 const Login = () => {
   return (
     <div>
-
-        Login
-
+        <form action="">
+            <h2 className="text-center text-6xl font-bold">Login</h2>
+            <textarea name="ename" id=""></textarea>
+        </form>
     </div>
   )
 }
