f5927f5ec6f7f1397c27cacf8bc86297232e4dff
Md Faisal Hussain <mdfaisalhussainm@gmail.com>
2026-01-21 10:05:17 +0530

    changes 2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>
-        <form action="">
+    <div className='flex  flex-col justify-center items-center borrder-radius-2xl m-auto '>
+        <form className='flex flex-col space-y-8 bg-blue-400 m-25 p-15' action="post">
             <h2 className="text-center text-6xl font-bold">Login</h2>
-            <textarea name="ename" id=""></textarea>
+            
+            <input className='border-1 p-0.5' type="text" placeholder="Email" />
+           
+            <input className='border-1 p-0.5' type="password" placeholder="Password" />
+            
+            <button className='border-zinc-400' type="submit">Login</button>
+            
         </form>
     </div>
   )
