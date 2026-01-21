26ba4bfac3626e674344a1eb3b745817f6304a11
Md Faisal Hussain <mdfaisalhussainm@gmail.com>
2026-01-21 10:21:11 +0530

    changes 3

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
         
           
 
-        <Login/>
+        
         <Register/>
         
         
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,8 +3,26 @@ import React from 'react'
 const Register = () => {
   return (
     <div>
-        
-     
+
+      <div>
+
+        <form action="post">
+
+          <h2>Registration Page</h2>
+
+          <textarea name="name" placeholder='name' id=""></textarea>
+
+          <textarea name="email" placeholder='email' id=""></textarea>
+
+          <textarea name="phone-number" placeholder='phone-number' id=""></textarea>
+
+          <textarea name="password" placeholder='password' id=""></textarea>
+
+          <button>Register</button>
+
+        </form>
+
+      </div>
 
     </div>
   )
